84a57340654a25fb9caf8ec25c7634383d9f6b14
Brendon Smith <brendonsmith@seacloud9.org>
2019-06-22 19:35:27 -0700

    adding callback

--- src/customComponents/DynamicTerrain.ts
+++ src/customComponents/DynamicTerrain.ts
@@ -74,7 +74,7 @@ export class DynamicTerrain {
     worldPosition: Vector3.Zero(), // vertex World position
     mapIndex: 0 | 0 // current map index
   }
-  private _intializedCallback:any
+  private _intializedCallback: any
   private _averageSubSizeX: number = 0.0 // map cell average x size
   private _averageSubSizeZ: number = 0.0 // map cell average z size
   private _terrainSizeX: number = 0.0 // terrain x size
@@ -98,7 +98,6 @@ export class DynamicTerrain {
   private static _norm: Vector3 = Vector3.Zero()
   private static _bbMin: Vector3 = Vector3.Zero()
   private static _bbMax: Vector3 = Vector3.Zero()
-
 
   /**
    * constructor
@@ -134,7 +133,7 @@ export class DynamicTerrain {
       SPmapData?: number[][] | Float32Array[]
       sps?: SolidParticleSystem
       SPcolorData?: number[][] | Float32Array[]
-      SPuvData?: number[][] | Float32Array[],
+      SPuvData?: number[][] | Float32Array[]
       intializedCallback?: any
     },
     scene: Scene
@@ -341,13 +340,12 @@ export class DynamicTerrain {
       spsNbPerType.push(count)
     }
     this.update(true) // recompute everything once the initial deltas are calculated
-    if(this._intializedCallback){
+    if (this._intializedCallback) {
       this.intialized()
     }
-    
   }
 
-  public intialized(){
+  public intialized() {
     this._intializedCallback(this)
   }
 
