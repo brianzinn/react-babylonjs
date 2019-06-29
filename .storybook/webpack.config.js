780497d09fe2e533c99b2f52f9ee1666afea5dc8
Brendon Smith <brendonsmith@seacloud9.org>
2019-06-29 14:13:34 -0700

    storybook improvements
    - starting to add parameters and theming within the config
    - toggled actions menu to be closed by default if you type `A` you can access it immediately
    - added storybook view source plugin so people can easily see how the example has been created
    - added a new story to demonstrate tweening with gasp

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -8,5 +8,11 @@ module.exports = async (obj) => {
           "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
         }
       }
+      obj.config.module.rules.push({
+        test: /\.stories\.jsx?$/,
+        loaders: [require.resolve('@storybook/addon-storysource/loader')],
+        enforce: 'pre',
+      });
+    
     return obj.config
   }
