24a2efcd98fda47efc7b15275c639214af1d25dd
Brian Zinn <github@wakeskate.com>
2019-10-04 00:07:42 -0700

    add physics to storybook. bump version for v2

--- stories/babylonjs/1-basic/babylonBasic.stories.js
+++ stories/babylonjs/1-basic/babylonBasic.stories.js
@@ -4,17 +4,17 @@ import { Engine, Scene } from '../../../dist/react-babylonjs.es5'
 import { Vector3 } from '@babylonjs/core/Maths/math'
 import '../../style.css'
 
-export default { title: 'Babylon Basic'}
-
-export const withDefaultPlayground = () => (
-  <div style={{ flex: 1, display: 'flex' }}>
-    <Engine antialias adaptToDeviceRatio canvasId='babylonJS' >
-      <Scene>
-        <freeCamera name='camera1' position={new Vector3(0, 5, -10)} setTarget={[Vector3.Zero()]} />
-        <hemisphericLight name='light1' intensity={0.7} direction={Vector3.Up()} />
-        <sphere name='sphere1' diameter={2} segments={16} position={new Vector3(0, 1, 0)} />
-        <ground name='ground1' width={6} height={6} subdivisions={2} />
-      </Scene>
-    </Engine>
-  </div>
+export default storiesOf('Babylon Basic', module)
+  .add('Default Playground', () => (
+    <div style={{ flex: 1, display: 'flex' }}>
+      <Engine antialias adaptToDeviceRatio canvasId='babylonJS' >
+        <Scene>
+          <freeCamera name='camera1' position={new Vector3(0, 5, -10)} setTarget={[Vector3.Zero()]} />
+          <hemisphericLight name='light1' intensity={0.7} direction={Vector3.Up()} />
+          <sphere name='sphere1' diameter={2} segments={16} position={new Vector3(0, 1, 0)} />
+          <ground name='ground1' width={6} height={6} subdivisions={2} />
+        </Scene>
+      </Engine>
+    </div>
+  )
 )
