74c9fe263b4aaa1460d07ed9ccdcc01d1d66d2f6
Brian Zinn <github@wakeskate.com>
2020-03-20 22:14:36 -0700

    Add edges-rendering example (import with side-effect)

---
+++ stories/babylonjs/1-basic/edgesRendering.stories.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Vector3, Color4 } from '@babylonjs/core';
+import '@babylonjs/core/Rendering/edgesRenderer' // You this need for side-effects
+import { storiesOf } from '@storybook/react'
+import { Engine, Scene } from '../../../dist/react-babylonjs'
+
+import '../../style.css';
+/**
+ * https://www.babylonjs-playground.com/#TYAHX#10
+ */
+export default storiesOf('Babylon Basic', module)
+  .add('Edges Rendering', () => (
+    <div style={{ flex: 1, display: 'flex' }}>
+      <Engine antialias adaptToDeviceRatio canvasId='babylonJS'>
+        <Scene>
+          <arcRotateCamera name='camera1' alpha={0} beta={Math.PI / 3} radius={10} target={Vector3.Zero()} />
+          <hemisphericLight name='light1' direction={Vector3.Up()} />
+          <box
+            name="box1"
+            onCreated={box => box.enableEdgesRendering()}
+            edgesWidth={4}
+            size={2}
+            position={new Vector3(0, 1.2, 0)}
+            edgesColor={new Color4(0, 0, 1, 1)}
+          />
+          <ground
+            onCreated={g => g.enableEdgesRendering()}
+            edgesWidth={3}
+            width={6}
+            height={6}
+            subdivisions={1}
+          />
+        </Scene>
+      </Engine>
+    </div>
+  )
+)
