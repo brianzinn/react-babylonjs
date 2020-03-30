cf8aeb5b86439d9384b8f7fc27d632004fc4909c
hookex <cookmycode@gmail.com>
2020-03-30 14:08:23 +0800

    feat[hooks]: add event hooks

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,7 +1,17 @@
-import { useContext, useEffect } from 'react';
-import { Nullable, Observer, Scene, EventState } from '@babylonjs/core';
+import {MutableRefObject, useContext, useEffect, useRef, useState} from 'react';
+import {
+  Nullable,
+  Observer,
+  Scene,
+  EventState,
+  ActionManager,
+  ActionEvent,
+  ExecuteCodeAction,
+  Mesh,
+} from '@babylonjs/core';
 
 import { SceneContext } from './Scene'
+import {CreatedInstance} from "./CreatedInstance";
 
 export type OnFrameRenderFn = (eventData: Scene, eventState: EventState) => void
 
@@ -22,4 +32,69 @@ export function useBeforeRender(callback: OnFrameRenderFn, mask?: number, insert
             }
         }
     })
+}
+
+
+export interface EventFunc {
+  (ev: ActionEvent): void;
+}
+
+export function useHover(over?: EventFunc, out?: EventFunc): [MutableRefObject<CreatedInstance<Mesh|null>>, boolean] {
+  const [value, setValue] = useState(false);
+
+  const ref = useRef<CreatedInstance<Mesh>>(null) as MutableRefObject<CreatedInstance<Mesh|null>>;
+
+  useEffect(() => {
+    if (ref.current) {
+      const mesh = ref.current.hostInstance as Mesh;
+
+      if (!mesh.actionManager) {
+        mesh.actionManager = new ActionManager(mesh.getScene());
+      }
+
+      mesh.actionManager.registerAction(
+        new ExecuteCodeAction(
+          ActionManager.OnPointerOverTrigger, function (ev) {
+            over && over(ev);
+            setValue(true);
+          }
+        )
+      );
+
+      mesh.actionManager.registerAction(
+        new ExecuteCodeAction(
+          ActionManager.OnPointerOutTrigger, function (ev) {
+            out && out(ev);
+            setValue(false);
+          }
+        )
+      );
+    }
+  }, [ref.current])
+
+  return [ref, value];
+}
+
+export function useClick(onClick: EventFunc): [MutableRefObject<CreatedInstance<Mesh|null>>]{
+  const ref = useRef<CreatedInstance<Mesh>>(null) as MutableRefObject<CreatedInstance<Mesh|null>>;
+
+  useEffect(() => {
+    if (ref.current) {
+      const mesh = ref.current.hostInstance as Mesh;
+
+      if (!mesh.actionManager) {
+        mesh.actionManager = new ActionManager(mesh.getScene());
+      }
+
+      mesh.actionManager.registerAction(
+        new ExecuteCodeAction(
+          ActionManager.OnPickTrigger, function (ev) {
+            onClick(ev);
+          }
+        )
+      );
+    }
+  }, [ref.current])
+
+  return [ref];
 }
