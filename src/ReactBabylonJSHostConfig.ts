117f5f7ec5ba7b6bc024b68164c32437733b7744
Hugo McPhee <mcphee.hugo@gmail.com>
2020-05-13 07:33:28 +1000

    Allows using react suspense without error (#63)
    * Allows react using suspense without error
    * fix mismatched types

--- src/ReactBabylonJSHostConfig.ts
+++ src/ReactBabylonJSHostConfig.ts
@@ -135,7 +135,10 @@ const ReactBabylonJSHostConfig: HostConfig<
   {},
   TimeoutHandler,
   NoTimeout
-> = {
+> & {
+   hideInstance: (instance: HostCreatedInstance<any>) => void;
+   unhideInstance: (instance: HostCreatedInstance<any>, props:Props) => void;
+} = {
   // This has the reconciler include in call chain ie: appendChild, removeChild
   get supportsMutation(): boolean {
     return true
@@ -425,6 +428,10 @@ const ReactBabylonJSHostConfig: HostConfig<
   shouldDeprioritizeSubtree: (type: string, props: Props): boolean => {
     return false
   },
+
+  hideInstance(instance: HostCreatedInstance<any>) {},
+
+  unhideInstance(instance: HostCreatedInstance<any>, props: Props) {},
 
   createTextInstance: (): any => {
     return undefined
