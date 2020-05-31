8c9aefbcaf1243b844f03ba4b23823f2a54a06b5
Brian Zinn <github@wakeskate.com>
2020-05-30 17:09:41 -0700

    Remove `react-spring` (and 14 other unused dependencies). #69

--- src/react-babylonjs.ts
+++ src/react-babylonjs.ts
@@ -3,10 +3,9 @@ export * from "./generatedProps";
 export * from "./hooks";
 export * from "./customComponents"; // TODO: Except for Skybox - these should not be exported.  they are internal.
 export * from "./PropsHandler";
+export { applyInitialPropsToInstance } from './UpdateInstance'; // Imported by react-babylonjs-spring
 
 export { default as Engine, withBabylonJS, BabylonJSContext, useBabylonEngine, useBabylonCanvas } from "./Engine";
 export { default as Scene, withScene, WithSceneContext, SceneContext, SceneEventArgs, useBabylonScene } from "./Scene";
 
 export { HostWithEvents } from "./customHosts";
-
-export * from './plugins/react-babylon-spring';
