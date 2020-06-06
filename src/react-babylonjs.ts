582c95dcd8a60395f9602c95fb3b67988ca1a85d
hookex <cookmycode@gmail.com>
2020-06-06 14:49:17 +0800

    fix: engine and canvas hooks is null

--- src/react-babylonjs.ts
+++ src/react-babylonjs.ts
@@ -6,8 +6,9 @@ export * from "./customComponents";
 export * from "./PropsHandler";
 export { applyInitialPropsToInstance } from './UpdateInstance'; // Imported by react-babylonjs-spring
 
-export { default as Engine, withBabylonJS, BabylonJSContext, useBabylonEngine, useBabylonCanvas } from "./Engine";
-export { default as Scene, withScene, WithSceneContext, SceneContext, SceneEventArgs, useBabylonScene } from "./Scene";
+export { default as Engine, withBabylonJS, BabylonJSContext,   } from "./Engine";
+export { default as Scene, withScene, WithSceneContext,
+  SceneContext, SceneEventArgs, useBabylonScene, useBabylonEngine, useBabylonCanvas } from "./Scene";
 
 export { HostWithEvents } from "./customHosts";
 
