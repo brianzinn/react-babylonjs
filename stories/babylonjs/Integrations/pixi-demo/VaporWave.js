8514af6a3db41f419782c7b7cbd91bc32de64ccb
Brendon Smith <brendonsmith@seacloud9.org>
2020-11-27 18:20:28 -0800

    upgrading the pixiJS demo adding a slider effect like https://tympanus.net/codrops/2019/12/16/scroll-refraction-and-shader-effects-in-three-js-and-react/

---
+++ stories/babylonjs/Integrations/pixi-demo/VaporWave.js
@@ -0,0 +1,47 @@
+
+import React, { useEffect } from 'react';
+import { Vector2, Mesh } from '@babylonjs/core';
+import { useBabylonScene } from '../../../../dist/react-babylonjs';
+import './shaders';
+let customProceduralTexture = null;
+let time = 0;
+
+const onCustomProceduralTextureCreated = (cpt) => {
+  customProceduralTexture = cpt; // assigning to reflection/refraction of mirrorball
+}
+
+const VaporWave = (props) => {
+    const scene = useBabylonScene();
+    useEffect(() => {
+        const observable = scene.onBeforeRenderObservable.add((scene) => {
+          if (scene !== null && customProceduralTexture !== null) {
+            time += scene.getEngine().getDeltaTime();
+            customProceduralTexture.setFloat("time", time);
+          }
+        })
+        return () => {
+          scene.onBeforeRenderObservable.remove(observable);
+        }
+      })
+
+    return (<>
+    <plane
+        name='vaporWareSunset'
+        width={20}
+        height={20}
+        billboardMode={Mesh.BILLBOARDMODE_ALL}>
+        <standardMaterial name='shaderMat'>
+        <customProceduralTexture
+            name='vaporWaveTexture'
+            texturePath="vaporWave"
+            size={512}
+            onCreated={onCustomProceduralTextureCreated}
+            assignTo='diffuseTexture'
+            setVector2={["resolution", new Vector2(1, 1)]}
+        />
+        </standardMaterial>
+    </plane>
+    </>)
+}
+
+export default VaporWave
--- stories/babylonjs/Integrations/pixi-render.stories.js
+++ stories/babylonjs/Integrations/pixi-render.stories.js
@@ -1,30 +1,29 @@
+
 import React, { useRef, useCallback, useState, useEffect } from 'react';
 import * as PIXI from 'pixi.js';
-import { Vector2, Vector3, Color3, Mesh } from '@babylonjs/core';
+import { Vector3, Color3 } from '@babylonjs/core';
 import { render as PixiRender, Text } from '@inlet/react-pixi'
 import { Engine, Scene, useBabylonScene } from '../../../dist/react-babylonjs';
-import '../../style.css';
-
-import {postSpectacularGradient, retroGoldGradient} from './pixi-styles';
-import './shaders'; // side-effect of registering a pixel shader
-
+import '@babylonjs/inspector'
+import {postSpectacularGradient, retroGoldGradient} from './pixi-demo/pixi-styles';
+import './pixi-demo/shaders'; // side-effect of registering a pixel shader
+import Slider from './pixi-demo/slide'
+import state from "./pixi-demo/store"
+import './pixi-demo/styles.css'
 export default { title: 'Integrations' };
 
 let customProceduralTexture = null;
 let time = 0;
 
-/**
- * This PIXI demo was modified (PIXI 5 and more declarative) from a seacloud9 fork + branch of this repo
- * https://github.com/seacloud9/react-babylonjs/tree/pixi-babylonv4.1-alpha-16
- */
 function ScenePIXI(props) {
   const beforeMount = useRef(false);
   const pixiRefs = useRef(undefined);
   const scene = useBabylonScene();
-
+  scene.clearColor = Color3.FromHexString('#04005E');
   if (beforeMount.current === false) {
     beforeMount.current = true;
     const engine = scene.getEngine();
+    //scene.debugLayer.show();
     const options = {
       autoStart: false,
       clearBeforeRender: false,
@@ -84,75 +83,56 @@ function ScenePIXI(props) {
   return null;
 }
 
-const onCustomProceduralTextureCreated = (cpt) => {
-  customProceduralTexture = cpt; // assigning to reflection/refraction of mirrorball
-}
-
-const onMirrorBallMaterialCreated = (mat) => {
-  mat.reflectionTexture = customProceduralTexture;
-  mat.refractionTexture = customProceduralTexture;
-}
-
-const SceneBabylonJS = (props) => <>
-  <freeCamera name='camera1' position={new Vector3(0, 5, -10)} setTarget={[new Vector3(0, 1.0, 1.0)]} />
-  <pointLight name='light2' position={new Vector3(0, 5, 0)} diffuse={Color3.FromHexString("#72CDFF")} specular={Color3.FromHexString("#FF00D5")} />
-  <hemisphericLight name='light1' intensity={0.7} direction={Vector3.Up()} diffuse={Color3.FromHexString("#D72AEA")} groundColor={Color3.FromHexString("#7528FB")} />
-  <plane
-    name='vaporWareSunset'
-    width={40}
-    height={40}
-    billboardMode={Mesh.BILLBOARDMODE_ALL}
-  >
-    <standardMaterial name='shaderMat'>
-      <customProceduralTexture
-        name='shaderToyTexture'
-        texturePath="shaderToy"
-        size={512}
-        onCreated={onCustomProceduralTextureCreated}
-        assignTo='diffuseTexture'
-        setVector2={["resolution", new Vector2(1, 1)]}
-      />
-    </standardMaterial>
-  </plane>
-  <sphere name='mirrorBall' diameter={2} segments={16} position={new Vector3(0, 1, 0)}>
-    <pbrMaterial
-      name='glass'
-      onCreated={onMirrorBallMaterialCreated}
-      linkRefractionWithTransparency
-      indexOfRefraction={0.52}
-      alpha={0}
-      directIntensity={0.0}
-      environmentIntensity={0.7}
-      cameraExposure={0.66}
-      cameraContrast={1.66}
-      microSurface={1}
-      reflectivityColor={new Color3(0.2, 0.2, 0.2)}
-      albedoColor={new Color3(0.85, 0.85, 0.85)}
-    />
-  </sphere>
+const SceneBabylonJSSlider = (props) => <>
+  <freeCamera name='camera1' position={new Vector3(0, 5, -10)} rotation={new Vector3(0, 0, 0)} />
+  <hemisphericLight name='light1' intensity={0.7} direction={Vector3.Up()} diffuse={Color3.FromHexString("#ffffff")} groundColor={Color3.FromHexString("#7528FB")} />
+  <Slider {...props} />
 </>
 
-const insertCoins = "Insert Coins";
 const postText = "P05T 5P3CTACU1AR DEMOSCENE";
 let engine = undefined;
-export const PIXIStory = () => {
+
+const scrollToArea = (spot) => {
+  document.querySelector('.scrollArea').scrollTo({
+    top: spot === 0 ? 0:spot * window.innerHeight,
+    left: 0,
+    behavior: 'smooth'
+  })
+}
+
+export const PIXIStory = ({ top, mouse }) => {
   const [found, setFound] = useState(false);
   const engineRef = useCallback(component => {
     engine = component;
     setFound(true);
   }, []);
 
+  const scrollArea = useRef()
+  const onScroll = e => {
+    return e.target && e.target.scrollTop ? state.top.current = e.target.scrollTop:false;
+  }
+  useEffect(() => void onScroll({ target: scrollArea.current }), [])
   return (
     <div style={{ flex: 1, display: 'flex' }}> 
       <Engine ref={engineRef} antialias adaptToDeviceRatio canvasId='babylonJS'>
         <Scene>
-          <SceneBabylonJS />
+          <SceneBabylonJSSlider {...state} />
           <ScenePIXI onEndRenderLoopObservable={engine?.onEndRenderLoopObservable} found={found}>
-            <Text text={insertCoins} x={(window.innerWidth / 2 - ((insertCoins.length / 2) * 40))} y={(window.innerHeight / 2 + 320)} style={retroGoldGradient} />
-            <Text text={postText} x={(window.innerWidth / 2 - ((postText.length / 2) * 32))} y={(window.innerHeight / 2 - 320)} style={postSpectacularGradient} />
+          <Text text={postText} x={(window.innerWidth / 2 - ((postText.length / 2) * 32))} y={(window.innerHeight / 2 - 320)} style={postSpectacularGradient} />
           </ScenePIXI>
         </Scene>
       </Engine>
+      <div className='slideContainer'>
+        Slides: 
+        <div className='btn' onClick={() => scrollToArea(0)}>1</div>
+        <div className='btn' onClick={() => scrollToArea(1.5)}>2</div>
+        <div className='btn' onClick={() => scrollToArea(2.2)}>3</div>
+        <div className='btn' onClick={() => scrollToArea(3.2)}>4</div>
+        <div className='btn' onClick={() => scrollToArea(4)}>5</div>
+      </div>
+      <div className="scrollArea" ref={scrollArea} onScroll={onScroll}>
+        <div style={{ height: `${state.pages * 100}vh` }} />
+      </div>
     </div>
   )
 }
