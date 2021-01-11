2b1eecbfaf50cc92a10410bf8605dfbd867d33d3
Brian Zinn <github@wakeskate.com>
2021-01-10 20:53:12 -0800

    Change color of NME block #105

--- stories/babylonjs/Basic/snippetMaterial.stories.js
+++ stories/babylonjs/Basic/snippetMaterial.stories.js
@@ -1,12 +1,12 @@
 import React, { useState, useCallback, useEffect } from 'react'
-import { Vector3, NodeMaterial } from '@babylonjs/core';
+import { Vector3, NodeMaterial, Color3 } from '@babylonjs/core';
 import '@babylonjs/core/Rendering/edgesRenderer' // You this need for side-effects
 import { Engine, Scene, useScene } from '../../../dist/react-babylonjs'
 import '../../style.css';
 
 export default { title: 'Babylon Basic' };
 
-const SnippetMaterialById = ({snippetId, name}) => {
+const SnippetMaterialById = ({snippetId, name, surfaceColor}) => {
   const scene = useScene();
   const [material, setMaterial] = useState(null);
   const parseMaterial = useCallback(async () => {
@@ -18,6 +18,13 @@ const SnippetMaterialById = ({snippetId, name}) => {
   }, [snippetId, scene]);
 
   useEffect(() => {
+    if (material) {
+      let block = material.getInputBlockByPredicate((b) => b.name === 'Surface Color');
+      block.value = Color3[surfaceColor]();
+    }
+  }, [surfaceColor]);
+
+  useEffect(() => {
     parseMaterial();
   }, [parseMaterial]);
 
@@ -26,19 +33,34 @@ const SnippetMaterialById = ({snippetId, name}) => {
   );
 };
 
-export const SnippetMaterial = () => (
-  <div style={{ flex: 1, display: 'flex' }}>
-    <Engine antialias adaptToDeviceRatio canvasId='babylonJS'>
-      <Scene>
-      <arcRotateCamera name="arc" target={Vector3.Zero()} minZ={0.001}
-        alpha={-Math.PI / 4} beta={(Math.PI / 4)} radius={5} upperBetaLimit={(Math.PI / 2) * 0.99}
-      />
-        <hemisphericLight name="light1" intensity={0.7} direction={Vector3.Up()} />
-        <sphere name="sphere1" diameter={2} segments={16} position={new Vector3(0, 1, 0)}>
-          <SnippetMaterialById name="sphereMat" snippetId="#81NNDY#20" />
-        </sphere>
-        <ground name="ground1" width={6} height={6} subdivisions={2} />
-      </Scene>
-    </Engine>
-  </div>
-)
+const colors = ["Red", "Green", "Yellow"]
+
+export const SnippetMaterial = () => {
+  const [selectedColor, setSelectedColor] = useState("Green");
+  const onChange = (e) => {
+    setSelectedColor(e.target.value);
+  }
+  return (
+    <>
+      <div style={{ flex: 1, display: 'flex' }}>
+        <select onChange={onChange}>
+          {colors.map(color => <option key={color} value={color} selected={selectedColor === color}>{color}</option>)}
+        </select>
+      </div>
+      <div style={{ flex: 1, display: 'flex' }}>
+        <Engine antialias adaptToDeviceRatio canvasId='babylonJS'>
+          <Scene>
+          <arcRotateCamera name="arc" target={Vector3.Zero()} minZ={0.001}
+            alpha={-Math.PI / 4} beta={(Math.PI / 4)} radius={5} upperBetaLimit={(Math.PI / 2) * 0.99}
+          />
+            <hemisphericLight name="light1" intensity={0.7} direction={Vector3.Up()} />
+            <sphere name="sphere1" diameter={2} segments={16} position={new Vector3(0, 1, 0)}>
+              <SnippetMaterialById name="sphereMat" snippetId="#81NNDY#20" surfaceColor={selectedColor} />
+            </sphere>
+            <ground name="ground1" width={6} height={6} subdivisions={2} />
+          </Scene>
+        </Engine>
+      </div>
+    </>
+  )
+}
