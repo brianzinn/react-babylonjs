bd4e71cd5360a4ecdc5efa035027292372e3771d
Brian Zinn <github@wakeskate.com>
2021-02-07 18:35:36 -0800

    github release notes (gren)

--- .grenrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    dataSource: 'commits',
-    prefix: '',
-    includeMessages: 'all',
-    ignoreCommitsWith: ['release.yml', ':package:'],
-    ignoreTagsWith: ['beta', 'v2.2.15', 'v3.0.1-rc.1'],
-    changelogFilename: 'docs/CHANGELOG.md'
-}
