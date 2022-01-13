fd9457f83fd869e2426d9bc9257b94defdfe0a28
Ben Allfree <ben@benallfree.com>
2022-01-12 18:09:25 -0800

    wip

--- static/content/basic/plain-old-react/HelloWorldComponent.tsx
+++
@@ -1,5 +0,0 @@
-import { FC } from 'react'
-
-const App: FC = () => <div style={{ flex: 1, display: 'flex' }}>Hello, React!</div>
-
-export default App
--- static/gatsby-config.js
+++ static/gatsby-config.js
@@ -1,50 +1,53 @@
-require("dotenv").config();
-const queries = require("./src/utils/algolia");
-const config = require("./config");
+require('dotenv').config()
+const queries = require('./src/utils/algolia')
+const config = require('./config')
 const plugins = [
   'gatsby-plugin-sitemap',
   'gatsby-plugin-sharp',
   {
     resolve: `gatsby-plugin-layout`,
     options: {
-        component: require.resolve(`./src/templates/docs.js`)
-    }
+      component: require.resolve(`./src/templates/docs.js`),
+    },
   },
   'gatsby-plugin-emotion',
   'gatsby-plugin-react-helmet',
   {
-    resolve: "gatsby-source-filesystem",
+    resolve: 'gatsby-source-filesystem',
     options: {
-      name: "docs",
-      path: `${__dirname}/content/`
-    }
+      name: 'docs',
+      path: `${__dirname}/content/`,
+    },
   },
   {
     resolve: 'gatsby-plugin-mdx',
     options: {
       gatsbyRemarkPlugins: [
         {
-          resolve: "gatsby-remark-images",
+          resolve: 'gatsby-remark-images',
           options: {
             maxWidth: 1035,
-            sizeByPixelDensity: true
-          }
+            sizeByPixelDensity: true,
+          },
         },
         {
-          resolve: 'gatsby-remark-copy-linked-files'
-        },
-        {
-          resolve: require.resolve(`./plugins/gatsby-remark-code-harness`),
+          resolve: 'gatsby-remark-copy-linked-files',
         },
         {
           resolve: 'remark-codesandbox/gatsby',
           options: {
             mode: 'iframe',
+            customTemplates: {
+              rbjs: {
+                extends: 'file:./codesandbox-template',
+                entry: './src/App.tsx',
+              },
+            },
           },
         },
       ],
-      extensions: [".mdx", ".md"]
-    }
+      extensions: ['.mdx', '.md'],
+    },
   },
   {
     resolve: `gatsby-plugin-gtag`,
@@ -57,9 +60,14 @@ const plugins = [
       anonymize: false,
     },
   },
-];
+]
 // check and add algolia
-if (config.header.search && config.header.search.enabled && config.header.search.algoliaAppId && config.header.search.algoliaAdminKey) {
+if (
+  config.header.search &&
+  config.header.search.enabled &&
+  config.header.search.algoliaAppId &&
+  config.header.search.algoliaAdminKey
+) {
   plugins.push({
     resolve: `gatsby-plugin-algolia`,
     options: {
@@ -67,28 +75,28 @@ if (config.header.search && config.header.search.enabled && config.header.search
       apiKey: config.header.search.algoliaAdminKey, // algolia admin key to index
       queries,
       chunkSize: 10000, // default: 1000
-    }}
-  )
+    },
+  })
 }
 // check and add pwa functionality
 if (config.pwa && config.pwa.enabled && config.pwa.manifest) {
   plugins.push({
-      resolve: `gatsby-plugin-manifest`,
-      options: {...config.pwa.manifest},
-  });
+    resolve: `gatsby-plugin-manifest`,
+    options: { ...config.pwa.manifest },
+  })
   plugins.push({
     resolve: 'gatsby-plugin-offline',
     options: {
       appendScript: require.resolve(`./src/custom-sw-code.js`),
     },
-  });
+  })
 } else {
-  plugins.push('gatsby-plugin-remove-serviceworker');
+  plugins.push('gatsby-plugin-remove-serviceworker')
 }
 
 // check and remove trailing slash
 if (config.gatsby && !config.gatsby.trailingSlash) {
-  plugins.push('gatsby-plugin-remove-trailing-slashes');
+  plugins.push('gatsby-plugin-remove-trailing-slashes')
 }
 
 module.exports = {
@@ -99,7 +107,10 @@ module.exports = {
     docsLocation: config.siteMetadata.docsLocation,
     ogImage: config.siteMetadata.ogImage,
     favicon: config.siteMetadata.favicon,
-    logo: { link: config.header.logoLink ? config.header.logoLink : '/', image: config.header.logo }, // backwards compatible
+    logo: {
+      link: config.header.logoLink ? config.header.logoLink : '/',
+      image: config.header.logo,
+    }, // backwards compatible
     headerTitle: config.header.title,
     githubUrl: config.header.githubUrl,
     helpUrl: config.header.helpUrl,
@@ -114,6 +125,6 @@ module.exports = {
     PRESERVE_WEBPACK_CACHE: false, // (Umbrella Issue (https://gatsby.dev/cache-clearing-feedback)) · Use webpack's persistent caching and don't delete webpack's cache when changing gatsby-node.js & gatsby-config.js files.
     PRESERVE_FILE_DOWNLOAD_CACHE: false, // (Umbrella Issue (https://gatsby.dev/cache-clearing-feedback)) · Don't delete the downloaded files cache when changing gatsby-node.js & gatsby-config.js files.
     PARALLEL_SOURCING: false, // EXPERIMENTAL · (Umbrella Issue (https://gatsby.dev/parallel-sourcing-feedback)) · Run all source plugins at the same time instead of serially. For sites with multiple source plugins, this can speedup sourcing and transforming considerably.
-    FUNCTIONS: false // EXPERIMENTAL · (Umbrella Issue (https://gatsby.dev/functions-feedback)) · Compile Serverless functions in your Gatsby project and write them to disk, ready to deploy to Gatsby Cloud
-  }
-};
+    FUNCTIONS: false, // EXPERIMENTAL · (Umbrella Issue (https://gatsby.dev/functions-feedback)) · Compile Serverless functions in your Gatsby project and write them to disk, ready to deploy to Gatsby Cloud
+  },
+}
