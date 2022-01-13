afbbefff4eae881ab6b3fc477791f7ca61cfa600
Ben Allfree <ben@benallfree.com>
2022-01-13 09:04:15 -0800

    wip

--- packages/static/src/components/mdxComponents/Stats.js
+++ packages/static/src/components/mdxComponents/Stats.js
@@ -1,5 +1,5 @@
-import React, { FC, useEffect, useState } from 'react'
-import { useBeforeRender, useCanvas, useEngine } from 'react-babylonjs'
+import { useEffect } from 'react'
+import { useEngine } from 'react-babylonjs'
 
 const Stats = (props) => {
   const engine = useEngine()
@@ -8,12 +8,14 @@ const Stats = (props) => {
     const update = () => {
       console.log('callback', engine.getFps())
       const ctx = engine.getRenderingCanvas().getContext('2d')
+
       if (ctx) {
         ctx.font = '48px serif'
         ctx.fillText('Hello world', 0, 0)
       }
       requestAnimationFrame(update)
     }
+
     requestAnimationFrame(update)
   })
 
