a71f6dc72dad0d14c1fc797e5ceccad180dc8efb
Viktor Zhurbin <vzhurbin@gmail.com>
2024-12-27 15:49:07 +0100

    adjust layout

--- website/src/components/Playground/Playground.tsx
+++ website/src/components/Playground/Playground.tsx
@@ -1,6 +1,14 @@
-import { SandpackProvider, SandpackLayout, SandpackCodeEditor } from '@codesandbox/sandpack-react'
+import {
+  SandpackProvider,
+  SandpackLayout,
+  SandpackCodeEditor,
+  SandpackStack,
+  SandpackPreview,
+} from '@codesandbox/sandpack-react'
 import { useDark } from 'rspress/runtime'
 import { Preview } from './Preview'
+import { defaultDependencies, dependencies } from './constants/dependencies'
+import { defaultFiles } from './constants/defaultFiles'
 
 export interface PlaygroundProps {
   files: string | Record<string, string>
@@ -11,14 +19,38 @@ export const Playground = (props: PlaygroundProps) => {
   const isDarkTheme = useDark()
   const theme = isDarkTheme ? 'dark' : 'light'
 
+  const deps = { ...dependencies, ...defaultDependencies }
   const files = typeof props.files === 'string' ? JSON.parse(props.files) : props.files
+  const firstFileName = Object.keys(props.files)[0]
 
   return (
-    <SandpackProvider template="react-ts" files={files} theme={theme}>
+    <SandpackProvider
+      template="react-ts"
+      files={{ ...files, ...defaultFiles }}
+      theme={theme}
+      customSetup={{
+        dependencies: deps,
+      }}
+      options={{
+        activeFile: firstFileName,
+      }}
+    >
       <SandpackLayout>
-        <SandpackCodeEditor wrapContent />
-        <Preview files={files} language={props.language} />
+        <SandpackCodeEditor wrapContent showTabs={false} showRunButton={false} />
+
+        <SandpackStack
+          style={{
+            height: 'var(--sp-layout-height)',
+          }}
+        >
+          <Preview files={files} language={props.language} />
+        </SandpackStack>
       </SandpackLayout>
+
+      {/* <SandpackLayout>
+        <SandpackCodeEditor />
+        <SandpackPreview />
+      </SandpackLayout> */}
     </SandpackProvider>
   )
 }
