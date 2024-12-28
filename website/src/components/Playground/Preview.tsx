be8ca1203caa73d952db35416d62de5b3701ed40
Viktor Zhurbin <vzhurbin@gmail.com>
2024-12-28 18:00:44 +0100

    support playground full screen

--- website/src/components/Playground/Preview.tsx
+++ website/src/components/Playground/Preview.tsx
@@ -1,18 +1,13 @@
-import {
-  OpenInCodeSandboxButton,
-  useActiveCode,
-  useTranspiledCode,
-} from '@codesandbox/sandpack-react'
+import { OpenInCodeSandboxButton, useActiveCode } from '@codesandbox/sandpack-react'
 import { Runner } from './Runner/Runner'
 import { PlaygroundProps } from './Playground'
 
-export const Preview = (props: PlaygroundProps) => {
-  const { language } = props
+export const Preview = (props: Pick<PlaygroundProps, 'language' | 'files'>) => {
   const { code } = useActiveCode()
 
   return (
     <>
-      <Runner language={language} code={code} />
+      <Runner language={props.language} code={code} />
       <div style={{ position: 'absolute', bottom: '8px', right: '8px' }}>
         <OpenInCodeSandboxButton />
       </div>
