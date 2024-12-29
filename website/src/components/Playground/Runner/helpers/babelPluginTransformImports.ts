7017455f6b5c2975fc80dba9debf1fb97e3d1a56
Viktor Zhurbin <vzhurbin@gmail.com>
2024-12-29 20:07:53 +0100

    added JSDoc comment for babel plugin

--- website/src/components/Playground/Runner/helpers/babelPluginTransformImports.ts
+++ website/src/components/Playground/Runner/helpers/babelPluginTransformImports.ts
@@ -2,6 +2,23 @@ import { createGetImport, createObjectPattern, createVariableDeclaration } from 
 import { Node } from '@babel/types'
 import { PluginItem } from '@babel/core'
 
+/**
+ * Turns this:
+ *  import { Animation, Vector3 } from '@babylonjs/core'
+ *  import React, { useEffect, useRef } from 'react'
+ *  import '@babylonjs/core/Engines/WebGPU/Extensions'
+ *  import { FallbackEngine, Scene, useScene } from 'react-babylonjs'
+ * Into this
+ *  var { Animation,  Vector3 } = __get_import("@babylonjs/core", false);
+ *  var React = __get_import("react", true);
+ *  var { useEffect, useRef } = __get_import("react", false);
+ *  var { FallbackEngine, useScene } = __get_import("react-babylonjs", false);
+ *
+ * Where `__get_import` is `getImport` function generated as
+ * a virtual module import inside of `pluginPlayground`.
+ * It makes makes all module imports in each demo file available
+ * through the RspackVirtualModulePlugin plugin
+ */
 export const babelPluginTransformImports = (): PluginItem => {
   let hasReactImported = false
 
