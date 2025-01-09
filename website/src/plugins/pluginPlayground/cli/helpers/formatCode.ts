0fbae0c8848cf91e53882a706e7582e5a51c5701
Viktor Zhurbin <vzhurbin@gmail.com>
2025-01-09 21:31:30 +0100

    add .prettierrc to make sure tsx and jsx have the same format

--- website/src/plugins/pluginPlayground/cli/helpers/formatCode.ts
+++ website/src/plugins/pluginPlayground/cli/helpers/formatCode.ts
@@ -1,16 +1,8 @@
 import prettier from 'prettier'
 
 const formatCode = async (code: string, filePath: string) => {
-  const fallbackConfig: prettier.Options = {
-    printWidth: 100,
-    singleQuote: true,
-    tabWidth: 2,
-    trailingComma: 'es5',
-    semi: false,
-  }
-
   // try to resolve .prettierrc config, or use a fallback
-  const config = (await prettier.resolveConfig(filePath)) ?? fallbackConfig
+  const config = (await prettier.resolveConfig(filePath)) ?? {}
 
   const result = await prettier.format(code, { ...config, parser: 'babel' })
 
