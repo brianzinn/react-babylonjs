1a9643ad21dd79279fea1a8ad091b9fcd1fbe1e5
Viktor Zhurbin <vzhurbin@gmail.com>
2025-01-09 12:56:57 +0100

    fix Open in Sandbox for JS

--- website/src/plugins/pluginPlayground/shared/constants.ts
+++ website/src/plugins/pluginPlayground/shared/constants.ts
@@ -7,5 +7,5 @@ export const ENTRY_FILE_BASE = 'App'
 
 export const EntryFiles: Record<Language, string> = {
   [Language.tsx]: `${ENTRY_FILE_BASE}.tsx`,
-  [Language.jsx]: `${ENTRY_FILE_BASE}.jsx`,
+  [Language.jsx]: `${ENTRY_FILE_BASE}.js`,
 }
